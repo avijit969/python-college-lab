1e278d91baa89e9b76a146021dcf584e2b6d0d67
Avijit <abhijitpradhan909@gmail.com>
2024-02-22 08:47:49 +0530

    assignment 03 to remove all duplicates from a given string

--- python_lab/ASSIGNMENT03/QUES06.py
+++ python_lab/ASSIGNMENT03/QUES06.py
@@ -1,1 +1,15 @@
 # WAP to remove all duplicates from a given string
+def remove_duplicate(string):
+    str = set()
+    result = []
+
+    for char in string:
+        if char not in str:
+            result.append(char)
+            str.add(char)
+    finalResult = "".join(result)
+    return finalResult
+
+
+string = input("Enter your string that you want to remove duplicate value :")
+print(f"After remove all duplicate value string is {remove_duplicate(string)}")
